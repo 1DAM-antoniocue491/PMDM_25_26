68fc00b15ef89daecb0cc216e701d6ddfe5932b3
unknown <2DAM-acuelop369@IESMARTINEZM.ES>
2025-09-26 19:21:48 +0200

    Actualización

--- actividades/app/src/main/java/com/mm/actividades/ejercicios/UD1/Actividad1/ejer2.kt
+++ actividades/app/src/main/java/com/mm/actividades/ejercicios/UD1/Actividad1/ejer2.kt
@@ -1,24 +1,39 @@
 package com.mm.actividades.ejercicios.UD1.Actividad1
 
+import android.provider.Settings
 import java.lang.Exception
 
 fun main() {
     val child = 5
-    val adult = 200
+    val adult = 45
     val senior = 89
 
     val isMonday = true
 
-    println("The movie ticket price for a person aged $child is ${ticketPrice(child, isMonday)}€.")
-    println("The movie ticket price for a person aged $adult is ${ticketPrice(adult, isMonday)}€.")
-    println("The movie ticket price for a person aged $senior is ${ticketPrice(senior, isMonday)}€.")
+    result(child, isMonday)
+    result(adult, isMonday)
+    result(senior, isMonday)
+}
+
+fun result (age: Int, isMonday: Boolean) {
+    val price : Int = ticketPrice(age, isMonday);
+
+    if (price != -1)
+        println("The movie ticket price for a person aged $age is ${price}€.")
+    else
+        System.err.println("La edad del espectador de $age años no es válida")
 }
 
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
     when (age) {
         in 0..12 -> return 15
-        in 13..60 -> return 25
+        in 13..60 -> {
+            if (isMonday)
+                return 5
+            else
+                return 30
+        }
         in 61 .. 100 -> return 20
-        else -> throw Exception("La edad del espectador no es válido")
+        else -> return -1
     }
 }
--- actividades/app/src/main/java/com/mm/actividades/ejercicios/UD1/Actividad1/ejer3.kt
+++ actividades/app/src/main/java/com/mm/actividades/ejercicios/UD1/Actividad1/ejer3.kt
@@ -1,2 +1,61 @@
 package com.mm.actividades.ejercicios.UD1.Actividad1
 
+import java.util.Scanner
+
+fun main() {
+    print("Indica la operación a realizar con el siguiente formato (C -> F 4.5): ")
+    val usuario : List<String> = readln().split(" ")
+
+    var result: Double
+    var number: Double = usuario.get(1).toDouble()
+
+    when (usuario.get(0)) {
+        in "C" -> {
+            when (usuario.get(2)) {
+                in "F" -> result = C_F(number)
+                in "K" -> result = C_K(number)
+            }
+        }
+        in "K" -> {
+
+        }
+    }
+}
+
+fun C_F(number: Double): Double {
+    return ((number*1.8)+32)
+}
+
+fun C_K(number: Double): Double {
+    return number+273.15
+}
+
+fun K_C(number: Double): Double {
+    return number-273.15
+}
+
+fun K_F(number: Double): Double {
+    return ((number-273.15)*1.8+32)
+}
+
+fun F_K(number: Double): Double {
+    return ((number+459.67)*(5/9))
+}
+
+fun F_C(number: Double): Double {
+    return (number - 32) * 1.8
+}
+
+/*
+Esto lo omito porqué no entiendo cómo es el tratado de los datos.
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
+ */
