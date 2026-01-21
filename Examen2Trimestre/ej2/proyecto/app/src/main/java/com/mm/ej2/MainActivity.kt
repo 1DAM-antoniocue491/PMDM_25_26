acc715744407ab0f630dbb80f86a9d583c707ab3
unknown <2DAM-acuelop369@IESMARTINEZM.ES>
2026-01-21 22:01:43 +0100

    Actualización

---
+++ Examen2Trimestre/ej2/proyecto/app/src/main/java/com/mm/ej2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mm.ej2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+
+	}
+
+}
